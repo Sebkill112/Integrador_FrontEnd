a76418603b77c302fe1fb1db0005bc49b5e5ce0f
Sebkill112 <snavarrosam45@gmail.com>
2023-09-29 11:28:56 -0500

    inicial

---
+++ src/views/login/index.jsx
@@ -0,0 +1,121 @@
+import { Avatar, Box, Button, CssBaseline, Grid, Link, Paper, TextField, ThemeProvider, Typography, createTheme } from '@mui/material';
+import React, { useState } from 'react';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Cookies from 'js-cookie';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const defaultTheme = createTheme();
+
+  const onchangeUser = (event) =>{
+    setUsername(event.target.value);
+  }
+  const onchangePass = (event) =>{
+    setPassword(event.target.value);
+  }
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:8090/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        // Si la autenticación es exitosa, el servidor debería haber configurado una cookie con el token JWT
+        const data = await response.json(); // Lee el contenido JSON de la respuesta
+      console.log(data);
+
+      } else {
+        // Maneja errores de autenticación
+        console.error('Error de inicio de sesión');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+       <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Ingreso 
+            </Typography>
+            <Box sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                value={username}
+                label="Usuario"
+                name="username"
+                onChange={onchangeUser}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={onchangePass}
+              />
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                onClick={handleLogin}
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Ingresar
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+};
+
+export default LoginPage;
