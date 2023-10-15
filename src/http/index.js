9421a609b494092f6bd88f3d9978577d403c41fc
Sebkill112 <snavarrosam45@gmail.com>
2023-10-15 15:22:17 -0500

    metodo http

--- src/http/index.js
+++ src/http/index.js
@@ -1,23 +1,43 @@
-import axios from 'axios'
+import axios from 'axios';
+const getJwtFromCookie = () => {
+  const cookieName = 'jwtToken';
+  const cookieValue = document.cookie
+    .split('; ')
+    .find((cookie) => cookie.startsWith(`${cookieName}=`));
 
-const Token = require('../http/token')
+  if (cookieValue) {
+    const token = cookieValue.split('=')[1];
 
-module.exports = {
-    post(body, header) {
-        axios.post(body, header, {
-            Authorization : `Bearer ${Token.obtenerToken()}`
-        })
-    },
+    return token;
+  }
 
-    put(body, header) {
-        axios.put(body, header, {
-            Authorization : `Bearer ${Token.obtenerToken()}`
-        })
+  return null;
+};
 
-    },
-    get(body, header) {
-        axios.get(body, header, {
-            Authorization : `Bearer ${Token.obtenerToken()}`
-        })
-    }
-}
+// Crea una instancia de Axios con la configuración base.
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8090', // Reemplaza con la URL de tu API
+});
+
+// Intercepta las solicitudes antes de enviarlas.
+axiosInstance.interceptors.request.use((config) => {
+  const token = getJwtFromCookie();
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+    config.headers['Content-Type'] = 'application/json';
+  }
+
+  return config;
+});
+
+
+
+// Crea el método HTTP global.
+const http = {
+  get: (url) => axiosInstance.get(url),
+  post: (url, data) => axiosInstance.post(url, data),
+  put: (url, data) => axiosInstance.put(url, data),
+};
+
+export default http;
